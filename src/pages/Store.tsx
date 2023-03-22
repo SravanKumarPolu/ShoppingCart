797b85c2e855ad4160605a8deb0bac086e6948e7
PVSKR <skr.polu@hotmail.com>
2023-03-22 14:22:21 +0530

    bootStrap Card
    Card img and  value of header and footer

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,12 +1,16 @@
 import { Col, Row } from "react-bootstrap";
+import { StoreItem } from "../components/StoreItem";
 import storeItems from "../data/items.json";
 export function Store() {
   return (
     <>
       <h1>Store</h1>
-      <Row>
+      <Row lg={3} md={2} xs={1} className="g-4 ">
         {storeItems.map((item) => (
-          <Col>{JSON.stringify(item)}</Col>
+          //   <Col className="p-4">{JSON.stringify(item)}</Col>
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
         ))}
       </Row>
     </>
