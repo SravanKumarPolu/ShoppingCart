2b179851c9f34d8f22fca00cb99add5754855080
PVSKR <skr.polu@hotmail.com>
2023-03-22 10:26:33 +0530

    Store page shows json items
    We are importing Json  items to  Store page.

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,14 @@
-export function Store(){
-    return <h1>Store</h1>
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+export function Store() {
+  return (
+    <>
+      <h1>Store</h1>
+      <Row>
+        {storeItems.map((item) => (
+          <Col>{JSON.stringify(item)}</Col>
+        ))}
+      </Row>
+    </>
+  );
 }
