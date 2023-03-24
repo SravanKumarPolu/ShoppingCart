9fcf4857064161a1466de25ef51c1f079534367f
PVSKR <skr.polu@hotmail.com>
2023-03-24 14:13:38 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,17 @@
-export function Home(){
-    return <h1>Home</h1>
+import { useState } from "react";
+
+export function Home() {
+  const [count, setCount] = useState(0);
+
+  const onIncrement = () => {
+    setCount(count + 1);
+    console.log(`count=${count}`);
+  };
+
+  return (
+    <>
+      <h1>Home</h1>
+      <button onClick={onIncrement}>Increment {count}</button>
+    </>
+  );
 }
