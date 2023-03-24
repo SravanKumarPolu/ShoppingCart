c42b4cf7266c41459b386ccb8138c8649b889235
PVSKR <skr.polu@hotmail.com>
2023-03-24 14:30:18 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,10 @@ import { useState } from "react";
 
 export function Home() {
   const [count, setCount] = useState(0);
-
   const onIncrement = () => {
     setCount(count + 1);
     console.log(`count=${count}`);
   };
-
   return (
     <>
       <h1>Home</h1>
