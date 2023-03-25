d2f7b124d16edc948aa193aa9e99942a21982cc8
PVSKR <skr.polu@hotmail.com>
2023-03-25 09:59:53 +0530

    Offcanvas  adding
     The Offcanvas is  feature of bootstrap . it is use to offscreen on desktop.

---
+++ src/components/ShoppingCart.tsx
@@ -0,0 +1,9 @@
+import { Offcanvas, OffcanvasHeader } from "react-bootstrap";
+
+export function ShoppingCart() {
+  return (
+    <Offcanvas>
+      <OffcanvasHeader>cart</OffcanvasHeader>
+    </Offcanvas>
+  );
+}
