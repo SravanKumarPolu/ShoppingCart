d5684c4c88edf8917410300f52e0827bdcbf1d59
PVSKR <skr.polu@hotmail.com>
2023-03-25 10:09:41 +0530

    Offcanvas testing 2

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -3,7 +3,9 @@ import { Offcanvas, OffcanvasHeader } from "react-bootstrap";
 export function ShoppingCart() {
   return (
     <Offcanvas show="true" placement="end">
-      <Offcanvas.Header>cart</Offcanvas.Header>
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Cart</Offcanvas.Title>
+      </Offcanvas.Header>
     </Offcanvas>
   );
 }
