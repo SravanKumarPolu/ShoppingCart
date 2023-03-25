f3d5eed479b9fbe45963d0c1068fa500611e6982
PVSKR <skr.polu@hotmail.com>
2023-03-25 16:02:48 +0530

    Offcanvas Testing level4
    The number  items in the cart can be display on the Shopping cart of Offcanvas.

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,15 +1,24 @@
+import { Children } from "react";
 import { Offcanvas } from "react-bootstrap";
-import { useShoppingCart } from "../context/ShoppingCartContext";
+import {
+  ShoppingCartProvider,
+  useShoppingCart,
+} from "../context/ShoppingCartContext";
 
 type ShoppingCartProps = {
   isOpen: boolean;
 };
 export function ShoppingCart({ isOpen }: ShoppingCartProps) {
   const { closeCart } = useShoppingCart();
+  const { openCart, cartQuantity } = useShoppingCart();
   return (
     <Offcanvas show={isOpen} onHide={closeCart} placement="end">
       <Offcanvas.Header closeButton>
-        <Offcanvas.Title>Cart</Offcanvas.Title>
+        <Offcanvas.Title>
+          {" "}
+          Cart
+          <Offcanvas.Body>{cartQuantity}</Offcanvas.Body>
+        </Offcanvas.Title>
       </Offcanvas.Header>
     </Offcanvas>
   );
