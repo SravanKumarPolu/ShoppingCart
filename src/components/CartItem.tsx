a96c1224d7262ea653e3372c0923623eae97c18b
PVSKR <skr.polu@hotmail.com>
2023-03-27 08:41:32 +0530

    Adding items name to offcanva of cart
    We are displaying name of items in the Offcanva-cart.

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -4,16 +4,23 @@ import storeItems from "../data/items.json";
 
 type CartItemProps = {
   id: number;
+  name: string;
   quantity: number;
 };
 
-export function CartItem({ id, quantity }: CartItemProps) {
+export function CartItem({ id, quantity, name }: CartItemProps) {
   const { removeFromCart } = useShoppingCart();
   const item = storeItems.find((i) => i.id === id);
+
   if (item == null) return null;
   return (
-    <Stack direction="horizontal" gap={2}>
+    <Stack
+      direction="horizontal"
+      gap={2}
+      className="d-flex justify-content-between"
+    >
       <img src={item.imgUrl} style={{ width: "64px", height: "65px" }}></img>
+      <div>{item.name}</div>
     </Stack>
   );
 }
