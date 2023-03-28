caabbf7ea44afb40edf39e1010b353c15f87fbb4
PVSKR <skr.polu@hotmail.com>
2023-03-28 08:50:04 +0530

    Update CartItem.tsx

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -16,32 +16,38 @@ export function CartItem({ id, quantity, name, price }: CartItemProps) {
 
   if (item == null) return null;
   return (
-    <Stack
-      direction="horizontal"
-      gap={2}
-      className="d-flex align-items-center justify-content-between"
-    >
-      <img
-        src={item.imgUrl}
-        style={{ width: "64px", height: "65px", objectFit: "cover" }}
-      ></img>
+    <>
+      <Stack
+        direction="horizontal"
+        gap={2}
+        className="d-flex align-items-center justify-content-between"
+      >
+        <img
+          src={item.imgUrl}
+          style={{ width: "64px", height: "65px", objectFit: "cover" }}
+        ></img>
 
-      <div className="me-atuod-flex align-items-center ">
-        <div>
-          {item.name}
-          {quantity > 1 && (
-            <span className="text-muted p-1 " style={{ fontSize: "15px" }}>
-              x{quantity}
-            </span>
-          )}
+        <div className="me-atuod-flex align-items-center ">
+          <div>
+            {item.name}
+            {quantity > 1 && (
+              <span className="text-muted p-1 " style={{ fontSize: "15px" }}>
+                x{quantity}
+              </span>
+            )}
+          </div>
+          <div className="s-2 text-muted" style={{ fontSize: "10px" }}>
+            {formatCurrency(item.price)}
+          </div>
         </div>
-        <div className="s-2 text-muted" style={{ fontSize: "10px" }}>
-          {formatCurrency(item.price)}
+        <div className="s-2 text-muted">
+          {formatCurrency(item.price * quantity)}
         </div>
-      </div>
+      </Stack>
       <div className="s-2 text-muted">
-        {formatCurrency(item.price * quantity)}
+        Total:
+        {formatCurrency((item.price += item.price))}
       </div>
-    </Stack>
+    </>
   );
 }
