c86aeb223192b80fa9ec34d10e2491c319161b90
PVSKR <skr.polu@hotmail.com>
2023-03-28 19:38:28 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,10 +6,23 @@ export function Home() {
     setCount(count + 1);
     console.log(`count=${count}`);
   };
+  const onDecrement = () => {
+    setCount(count - 1);
+    console.log(`count=${count}`);
+  };
   return (
     <>
       <h1>Home</h1>
-      <button onClick={onIncrement}>Increment {count}</button>
+
+      <div className="d-flex flex-row ">
+        <button onClick={onIncrement} className="  btn btn-primary  p-2 m-4 ">
+          Increment
+        </button>
+        <h2> count:{count}</h2>
+        <button onClick={onDecrement} className="btn  btn-danger p-1 m-4">
+          Dcrement{" "}
+        </button>
+      </div>
     </>
   );
 }
