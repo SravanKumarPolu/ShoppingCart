2697fd5bc8528f44fc6e17350fe62263687eab6f
PVSKR <skr.polu@hotmail.com>
2023-03-29 10:51:55 +0530

    Update Chart.tsx

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,20 +1,33 @@
-import React from "react";
-import { BarChart, Bar, XAxis, YAxis } from "recharts";
+import { Bar, BarChart, XAxis, YAxis } from "recharts";
 
 const data = [
-  { name: "Jan", value: 10 },
-  { name: "Feb", value: 15 },
-  { name: "Mar", value: 8 },
-  { name: "Apr", value: 20 },
-  { name: "May", value: 12 },
+  {
+    name: "raj",
+    value: 10,
+  },
+  {
+    name: "madhu",
+    value: 30,
+  },
+  {
+    name: "venky",
+    value: 60,
+  },
+  {
+    name: "kumar",
+    value: 29,
+  },
 ];
-
-const Chart = () => (
-  <BarChart width={400} height={300} data={data}>
-    <XAxis dataKey="name" />
-    <YAxis />
-    <Bar dataKey="value" fill="#8884d8" />
-  </BarChart>
-);
-
-export default Chart;
+type Data = {
+  name: "string";
+  value: number;
+};
+export default function Chart() {
+  return (
+    <BarChart width={359} height={280} data={data}>
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Bar dataKey="value" fill="#8884d8" />
+    </BarChart>
+  );
+}
