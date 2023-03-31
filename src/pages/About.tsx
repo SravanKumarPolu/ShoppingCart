afda874f18b4ead2c4e1fd51e1fc9b9f56f66be0
PVSKR <skr.polu@hotmail.com>
2023-03-31 18:14:18 +0530

    Checking1

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,12 +3,17 @@ import { CartItemChart } from "../components/CartItemChart";
 import { ShoppingCartChart } from "../components/ShoppingCartChart";
 import { Children } from "react";
 import { formatCurrency } from "../utilities/formatCurrency";
+import {
+  useShoppingCart,
+  ShoppingCartContextChart,
+} from "../context/ShoppingCartContextChart";
 
 export function About() {
+  const { openCart, cartQuantity } = useShoppingCart();
   return (
     <>
       <Chart />
-      <ShoppingCartChart isOpen={true} />
+      {cartQuantity > 0 && <div>about{cartQuantity}</div>}
     </>
   );
 }
