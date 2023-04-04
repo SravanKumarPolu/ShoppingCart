3514ab836ad3bbe8d3c13790103e543328b6bc60
PVSKR <skr.polu@hotmail.com>
2023-04-04 15:58:03 +0530

    grad has added to home

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import Chart from "../charts/Chart";
 import { CartItemChart } from "../components/CartItemChart";
 
 export function About() {
+  const [count, setCount] = useState(0);
+  const onIncrement = () => {
+    setCount(count + 1);
+    console.log(`count=${count}`);
+  };
+  const onDecrement = () => {
+    setCount(count - 1);
+    console.log(`count=${count}`);
+  };
   return (
     <>
-      <Chart />
-
-      <CartItemChart name={""} price={0} key={item.id} {...item} />
+      <CartItemChart name={""} price={0} id={0} quantity={0} />
+      <div className="d-flex flex-row ">
+        <button onClick={onIncrement} className="  btn btn-primary  p-2 m-4 ">
+          Increment
+        </button>
+        <h2> count:{count}</h2>
+        <button onClick={onDecrement} className="btn  btn-danger p-1 m-4">
+          Dcrement{" "}
+        </button>
+      </div>
     </>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,14 @@
+import Chart from "../charts/Chart";
 import { useState } from "react";
 
 export function Home() {
-  const [count, setCount] = useState(0);
-  const onIncrement = () => {
-    setCount(count + 1);
-    console.log(`count=${count}`);
-  };
-  const onDecrement = () => {
-    setCount(count - 1);
-    console.log(`count=${count}`);
-  };
   return (
     <>
-      <h1>Home</h1>
-
-      <div className="d-flex flex-row ">
-        <button onClick={onIncrement} className="  btn btn-primary  p-2 m-4 ">
-          Increment
-        </button>
-        <h2> count:{count}</h2>
-        <button onClick={onDecrement} className="btn  btn-danger p-1 m-4">
-          Dcrement{" "}
-        </button>
+      <h1 className="text-warning">Home</h1>
+      <div
+        style={{ background: "linear-gradient( to right,palegreen, orange )" }}
+      >
+        <Chart />
       </div>
     </>
   );
